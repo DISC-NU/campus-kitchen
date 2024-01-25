bb6b4e225dfe8c8dd3d508cf49e21583b1fb1980
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-01-25 13:39:52 -0600

    db

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,39 +4,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"database/sql"
 
 	"github.com/go-chi/chi/v5"
-	_ "github.com/lib/pq"
-	// "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
-func connectToDB() *sql.DB {
-    connStr := "user=postgres password=postgres dbname=localhost sslmode=disable"
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatal(err)
-    }
-	fmt.Println("Connected to DB")
-    return db
-}
-
 func main() {
-	db := connectToDB()
-    defer db.Close()
+	// Load connection string from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load env", err)
+	}
 
+	// Open a connection to PlanetScale
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	rows, err := db.Query("SHOW TABLES")
+	if err != nil {
+		log.Fatalf("failed to query: %v", err)
+	}
+	defer rows.Close()
+
+	var tableName string
+	for rows.Next() {
+		if err := rows.Scan(&tableName); err != nil {
+			log.Fatalf("failed to scan row: %v", err)
+		}
+		log.Println(tableName)
+	}
+
+	defer db.Close()
 	r := chi.NewRouter()
-	// r.Use(middleware.Logger)
-	// r.Use(middleware.RequestID)
-	// r.Use(middleware.RealIP)
-	// r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to the API"))
 	})
 
 	fmt.Println("Server running on port 3000")
-	
+
 	http.ListenAndServe(":3000", r)
 }
