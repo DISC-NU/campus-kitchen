2929ab64e8345405dafbb9a6022d09301e719284
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-02-06 11:41:10 -0600

    get user sql

---
+++ backend/db/migrations/000002_rename_user_table.up.sql
@@ -0,0 +1,1 @@
+RENAME TABLE Person TO users;
---
+++ backend/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    volunteer_name VARCHAR(255) NOT NULL,
+    volunteer_type ENUM('volunteer', 'shift_lead') NOT NULL,
+    total_hours_volunteered FLOAT NOT NULL,
+    total_pounds_food_recovered FLOAT NOT NULL,
+    total_number_meals_given FLOAT NOT NULL,
+    PRIMARY KEY (volunteer_name)
+);
