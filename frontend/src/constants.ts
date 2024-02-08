88b6da48af259bf4fc10b2c52fffe6fd2bb0bcc4
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-02-07 19:56:22 -0600

    changes

---
+++ frontend/src/constants.ts
@@ -0,0 +1,3 @@
+export const REDIRECT_URL = "http://127.0.0.1:8080/auth/google/callback";
+export const GOOGLE_OAUTH_CLIENT_ID =
+  "519346730061-q5i3t15ji2r3fsrt88od7gsn92c0bhv8.apps.googleusercontent.com";
---
+++ frontend/src/util/getGoogleOauthUrl.ts
@@ -0,0 +1,23 @@
+import { GOOGLE_OAUTH_CLIENT_ID, REDIRECT_URL } from "../constants";
+
+export const getGoogleUrl = (state: string) => {
+  const rootUrl = `https://accounts.google.com/o/oauth2/v2/auth`;
+
+  const options = {
+    redirect_uri: REDIRECT_URL,
+    client_id: GOOGLE_OAUTH_CLIENT_ID,
+    access_type: "offline",
+    response_type: "code",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ].join(" "),
+    state: JSON.stringify(state),
+  };
+
+  const qs = new URLSearchParams(options);
+
+  return `${rootUrl}?${qs.toString()}`;
+};
+
