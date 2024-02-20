961aa1dc07217aabd72fc2a560abc3de3ce99c66
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-02-19 20:06:51 -0600

    changes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,14 +8,13 @@ export default function Home() {
       fetch("http://127.0.0.1:8080/users/me", {
         method: "GET",
         credentials: "include",
-      }).then((res) =>
-        res.json()
-      ),
+      }).then((res) => res.json()),
   });
-  console.log(data)
+  console.log(data);
   return (
     <>
       <h1>Home</h1>
+      <div>user: {JSON.stringify(data) ?? "not logged in"}</div>
       <a href={getGoogleUrl("/")}>Login with Google</a>
     </>
   );
